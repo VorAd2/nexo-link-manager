800f9d3a208a6c808a6e2c5bb3facef4aa461d3b
Vitor <adrianovor@gmail.com>
2025-10-09 13:46:13 -0300

    Corrigindo erro na detecção de cores em FileCard 1) Substituindo interpolações de string por ternários na classe tailwind

--- frontend/src/components/FileCard.tsx
+++ frontend/src/components/FileCard.tsx
@@ -8,38 +8,23 @@ interface FileCardProps {
 }
 
 export default function FileCard({ variant, label, expiration }: FileCardProps) {
-    let background
-    let primary
-    let secondary
-
-    switch (variant) {
-        case 'active':
-            background = 'bg-green-950'
-            primary = 'green-400'
-            secondary = 'green-100'
-            break
-        case 'expired':
-            background = 'bg-orange-950'
-            primary = 'orange-400'
-            secondary = 'orange-100'
-    }
 
     return (
         <div
             className={`
                 flex flex-col justify-center items-center
                 p-4 
-                ${background} 
-                border-2 rounded-4xl border-${primary} 
-                w-40 h-56
-                text-${secondary}    
+                ${variant === 'active' ? 'bg-green-950' : 'bg-orange-950'} 
+                border-2 rounded-4xl 
+                ${variant === 'active' ? 'border-green-400' : 'border-orange-400'}
+                w-40 h-56    
             `}
         >
             <header>
-                <span className={`text-${primary}`}><FolderClosed /></span>
-                <h2 className={`text-${secondary}`}>{label}</h2>
+                <span className={variant === 'active' ? 'text-green-400' : 'text-orange-400'}><FolderClosed /></span>
+                <h2 className={variant === 'active' ? 'text-green-100' : 'text-orange-100'}>{label}</h2>
             </header>
-            <p className={`text-${primary}`}>{expiration}</p>
+            <p className={variant === 'active' ? 'text-green-400' : 'text-orange-400'}>{expiration}</p>
         </div>
     )
 }

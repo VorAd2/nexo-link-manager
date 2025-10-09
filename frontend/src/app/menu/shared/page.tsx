f3c14d5042ed56b657cf7ab1c489e3a00ab212a9
Vitor <adrianovor@gmail.com>
2025-10-09 17:40:17 -0300

    Estrutura definida e responsividade em Menu/Upload

--- frontend/src/app/menu/shared/page.tsx
+++ frontend/src/app/menu/shared/page.tsx
@@ -1,19 +1,71 @@
+'use client'
+//window so existe no lado do client
+
+import { useEffect, useState } from "react"
 import FileCard from "@/components/FileCard"
 
 export default function SharedPage() {
+    const [length, setLength] = useState(2)
+
+    const activeList = Array.from({ length: length }).map((_, i) =>
+        <FileCard key={i} variant="active" label="Contrato" expiration="Expira em 24 min" />
+    )
+    const expiredList = Array.from({ length: length }).map((_, i) =>
+        <FileCard key={i} variant="expired" label="Multa trânsito" expiration="Expira em 20 dias" />
+    )
+
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth < 500) setLength(2)
+            else if (window.innerWidth < 1000) setLength(3)
+            else if (window.innerWidth < 1200) setLength(4)
+            else if (window.innerWidth < 1920) setLength(5)
+            else return setLength(6)
+        }
+        handleResize()
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener("resize", handleResize)
+    }, [])
+
     return (
         <div className="flex flex-1">
-            <div className="py-16 px-20 grid grid-cols-1">
+            <div className="
+                grid grid-cols-1 w-full 
+                py-8 px-12 xl:py-16 xl:px-20
+            ">
                 <section>
-                    <h1 className="text-green-100 text-4xl ml-5 mb-6">ATIVOS</h1>
-                    <div className="">
-                        <FileCard variant="active" label="Contrato" expiration="Expira em 24 min" />
+                    <h1 className="
+                    text-green-100 text-2xl sm:text-3xl xl:text-4xl
+                    ml-2 mb-4 xl:ml-5 xl:mb-6
+                    ">ATIVOS</h1>
+                    <div className="
+                        flex flex-row space-x-10 
+                        ml-1 xl:ml-0
+                    "
+                    >
+                        {activeList}
+                    </div>
+                    <div className="
+                        flex justify-center items-center 
+                        mt-8 PAGINATION
+                    "
+                    >
+                        <span className="text-white text-xl">1..2..3</span>
                     </div>
                 </section>
-                <section>
-                    <h1 className="text-orange-100 text-4xl ml-5 mb-6">INATIVOS</h1>
-                    <div>
-                        <FileCard variant="expired" label="Papéis divórcio" expiration="Expirado há 20 dias" />
+                <section className="mt-8">
+                    <h1 className="
+                    text-orange-100 text-2xl sm:text-3xl xl:text-4xl 
+                    ml-2 mb-4 xl:ml-5 xl:mb-6
+                    ">INATIVOS</h1>
+                    <div className="
+                    flex flex-row space-x-10 
+                    ml-1 xl:ml-0
+                    ">
+                        {expiredList}
+                    </div>
+                    <div className="flex justify-center items-center mt-8 PAGINATION">
+                        <span className="text-white text-xl">1..2..3</span>
                     </div>
                 </section>
             </div>
--- frontend/src/components/FileCard.tsx
+++ frontend/src/components/FileCard.tsx
@@ -12,19 +12,35 @@ export default function FileCard({ variant, label, expiration }: FileCardProps) 
     return (
         <div
             className={`
-                flex flex-col justify-center items-center
-                p-4 
+                flex flex-col justify-center items-center space-y-2
+                p-4
                 ${variant === 'active' ? 'bg-green-950' : 'bg-orange-950'} 
                 border-2 rounded-4xl 
                 ${variant === 'active' ? 'border-green-400' : 'border-orange-400'}
-                w-40 h-56    
+                w-28 h-44 sm:w-32 sm:h-46 xl:w-36 xl:h-48 3xl:w-40 3xl:h-52 4xl:w-44 4xl:h-54
             `}
         >
-            <header>
-                <span className={variant === 'active' ? 'text-green-400' : 'text-orange-400'}><FolderClosed /></span>
-                <h2 className={variant === 'active' ? 'text-green-100' : 'text-orange-100'}>{label}</h2>
+            <header className="
+                flex flex-col justify-center items-center 
+                space-y-2 
+                text-wrap
+            ">
+                <span className={variant === 'active' ? 'text-green-400' : 'text-orange-400'}>
+                    <FolderClosed className="size-14 sm:size-16 xl:size-18 3xl:size-20" strokeWidth={1.5} />
+                </span>
+                <h2 className={`
+                        ${variant === 'active' ? 'text-green-100' : 'text-orange-100'}
+                        text-lg sm:text-xl xl:text-2xl
+                        text-center line-clamp-2 wrap-anywhere max-w-[9rem]
+                    `}>{label}</h2>
             </header>
-            <p className={variant === 'active' ? 'text-green-400' : 'text-orange-400'}>{expiration}</p>
+            <p className={`
+                ${variant === 'active' ? 'text-green-200' : 'text-orange-200'}
+                text-xs sm:text-sm xl:text-md 3xl:text-lg
+            `}
+            >
+                {expiration}
+            </p>
         </div>
     )
 }
